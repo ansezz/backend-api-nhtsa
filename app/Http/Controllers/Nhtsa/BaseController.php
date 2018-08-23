438eaab0454c8ed73fea6f907b2e82e2fcfde73e
Anass Ez-Zouaine <ans-ezzouaine@hotmail.com>
2018-08-23 17:02:04 +0100

    Create base controller

---
+++ app/Http/Controllers/Nhtsa/BaseController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Nhtsa;
+
+use App\Http\Controllers\Controller;
+
+class BaseController extends Controller
+{
+
+    public function __construct()
+    {
+    }
+}
