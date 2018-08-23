a40d00ed17f065e51ca963eb127b2744de4fb6ca
Anass Ez-Zouaine <ans-ezzouaine@hotmail.com>
2018-08-23 17:43:24 +0100

    remove api prefix.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,16 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-Route::namespace('Nhtsa')->group(function () {
-    Route::get('vehicles/{model_year}/{manufacturer}/{model}', 'SafetyRatingsController@vehicles');
+Route::get('/', function () {
+    return view('welcome');
 });
