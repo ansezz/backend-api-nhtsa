1f262eece5ff2a51b991bb30e005e65e0d138f4b
Anass Ez-Zouaine <ans-ezzouaine@hotmail.com>
2018-08-24 01:01:20 +0100

    :white_check_mark: Adding tests for Requirement `2` endpoint POST `/vehicles`.

--- tests/Feature/Nhtsa/SafetyRatingsTest.php
+++ tests/Feature/Nhtsa/SafetyRatingsTest.php
@@ -7,11 +7,11 @@ use Tests\TestCase;
 class SafetyRatingsTest extends TestCase
 {
     /**
-     * A basic test example.
+     * A vehicles With GET test.
      *
      * @return void
      */
-    public function modelYear()
+    public function testVehiclesWithGet()
     {
         $response = $this->get('/vehicles/2015/Audi/A3');
 
@@ -19,6 +19,51 @@ class SafetyRatingsTest extends TestCase
             ->assertJsonStructure([
                 'Count',
                 'Results'
+            ]);
+
+    }
+
+    /**
+     * A vehicles With POST test .
+     *
+     * @return void
+     */
+    public function testVehiclesWithPost()
+    {
+        $payload = [
+            'modelYear' => 2015,
+            'manufacturer' => 'Audi',
+            'model' => 'A3'
+        ];
+
+        $response = $this->json('POST', '/vehicles', $payload);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'Count',
+                'Results'
+            ]);
+
+    }
+
+
+    /**
+     * A vehicles With POST validation test .
+     *
+     * @return void
+     */
+    public function testVehiclesWithPostValidation()
+    {
+        $payload = [
+            'modelYear' => 2015
+        ];
+
+        $response = $this->json('POST', '/vehicles', $payload);
+
+        $response->assertStatus(422)
+            ->assertJsonStructure([
+                'message',
+                'errors'
             ]);
 
     }
