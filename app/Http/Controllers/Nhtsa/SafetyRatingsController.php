70a3e9ffd13e09da5d76901fdf2639266b881fe0
Anass Ez-Zouaine <ans-ezzouaine@hotmail.com>
2018-08-26 20:37:01 +0100

    add swagger annotation

--- app/Http/Controllers/Nhtsa/SafetyRatingsController.php
+++ app/Http/Controllers/Nhtsa/SafetyRatingsController.php
@@ -22,6 +22,50 @@ class SafetyRatingsController extends BaseController
 
 
     /**
+     * @OA\Info(title="Backend API NHTSA", version="0.1")
+     */
+
+    /**
+     * @OA\Get(
+     *   path="/vehicles/{model_year}/{manufacturer}/{model}",
+     *   summary="Requirement 1 & 3",
+     *   description="Get available vehicle variants for a selected Model Year, Make and Model",
+     * @OA\Parameter(
+     *         name="model_year",
+     *         in="path",
+     *         description="model year of vehicle",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int64",
+     *             example=2015
+     *         )
+     *     ),
+     * @OA\Parameter(
+     *         name="manufacturer",
+     *         in="path",
+     *         description="manufacturer of vehicle",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string",
+     *             example="Audi"
+     *         )
+     *     ),
+     * @OA\Parameter(
+     *         name="model",
+     *         in="path",
+     *         description="model of vehicle",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string",
+     *             example="A3"
+     *         )
+     *     ),
+     * @OA\Response(
+     *     response=200,
+     *     description="A list with vehicles"
+     *   )
+     * )
      * @param $model_year
      * @param $manufacturer
      * @param $model
@@ -33,6 +77,39 @@ class SafetyRatingsController extends BaseController
     }
 
     /**
+     * @OA\Post(
+     *   path="/vehicles",
+     *   summary="Requirement 2",
+     *   description="Get available vehicle variants for a selected Model Year, Make and Model",
+     *   @OA\RequestBody(
+     *       required=true,
+     *       @OA\MediaType(
+     *           mediaType="application/x-www-form-urlencoded",
+     *           @OA\Schema(
+     *               type="object",
+     *               @OA\Property(
+     *                   property="model",
+     *                   description="model of vehicle",
+     *                   type="string"
+     *               ),
+     *               @OA\Property(
+     *                   property="manufacturer",
+     *                   description="manufacturer of vehicle",
+     *                   type="string"
+     *               ),
+     *               @OA\Property(
+     *                   property="model_year",
+     *                   description="model year of vehicle",
+     *                   type="integer"
+     *               ),
+     *           )
+     *       )
+     *   ),
+     * @OA\Response(
+     *     response=200,
+     *     description="A list with vehicles"
+     *   )
+     * )
      * @param VehiclesPostRequest $request
      * @return \Illuminate\Http\JsonResponse
      */
